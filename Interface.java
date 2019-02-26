90152f59f1372646995ecf9e6c5ba7110dc37498
anniewit <annemarie.witschas@gmail.com>
2019-02-27 00:44:16 +0100

    jetzt auch mit extra parameter für Local-beam und random-restart und fehler meldungen

--- Interface.java
+++ Interface.java
@@ -17,45 +17,68 @@ public class Interface {
     static String [] algorithmChoices = {"Hill-Climbing", "First-Choice Hill-Climbing", "Random-Restart Hill-Climbing", "Local-Beam Search", "Simulated Annealing"};
     static JComboBox cBox = new JComboBox<String>(algorithmChoices);
 
-
+    //Ausrichtung: nicht so wichtig
     public final static boolean RIGHT_TO_LEFT = false;
-
     public static void addComponentsToPane(Container pane) {
         if (RIGHT_TO_LEFT) {
             pane.setComponentOrientation(
                 ComponentOrientation.RIGHT_TO_LEFT);
         }
 
+        // function for run-button: calls method and hands over files and which algorithm
         JButton runButton = new JButton("Run");
         runButton.addActionListener(new ActionListener(){
         
-            //@Override
             public void actionPerformed(ActionEvent e) {
-
+                //collect necessary info for starting search (both selected files and the selected algorithm)
                 String selectedAlg = (String)cBox.getSelectedItem();
                 String warehousePath = warehouseLabel.getText();
                 String orderPath = orderLabel.getText();
                 orderLabel.setText(selectedAlg);
-/* 
-                //for Testing:
+                int repetition = -1;
+
+                //opens pop-up to specify the repetitions for Local beam search and RR Hill-climbing
+                if (selectedAlg == "Local-Beam Search" || selectedAlg == "Random-Restart Hill-Climbing"){
+                    // opens pop up, gets input from user
+                    String input = JOptionPane.showInputDialog("Please specify the number of times the search should run: ");
+                    
+                    // if input is not an int, show error message
+                    try{
+                        repetition = Integer.parseInt(input);
+                        
+                    } catch(NumberFormatException ex){
+                        JOptionPane.showMessageDialog(null, "Please only enter an integer greater than 1.");
+                    } 
+                    if (repetition < 1){
+                        JOptionPane.showMessageDialog(null, "Please enter a number greater than 1.");
+                    }
+                    
+                }
+
+                //check if both files were inserted
+                if (warehousePath == "" || orderPath == ""){
+                    JOptionPane.showMessageDialog(null, "Please enter an order and warehouse file.");
+                } else{
+                     Preprocessing.prepare(warehousePath, orderPath, selectedAlg, repetition);
+                }
+ 
+/*                 //for Testing:
                 System.out.println(selectedAlg);
                 System.out.println(warehousePath);
-                System.out.println(orderPath); */
+                System.out.println(orderPath); 
+                System.out.println(repetition);
+ */
 
-                //for next steps:
-                PrepareSearch.prepare(warehousePath, orderPath, selectedAlg);
             }
         });
 
-
+        //makes a pop-up window to select a file
         JFileChooser chooser = new JFileChooser();
 
         // Action for the warehouse button:  opens dialog to select a file, print path
-        JButton warehouseSelector = new JButton("Select: ");
-        //JLabel warehouseLabel = new JLabel();
+        JButton warehouseSelector = new JButton("Select");
         warehouseSelector.addActionListener(new ActionListener(){
         
-
            public void actionPerformed(ActionEvent e) {
                 chooser.showDialog(pane, "Select as Warehouse File");
 
@@ -71,15 +94,12 @@ public class Interface {
        );
         
         // Action for the order button: opens dialog to select a file, print path
-        // JLabel orderLabel = new JLabel();
-        JButton orderSelector = new JButton("Select: ");
+        JButton orderSelector = new JButton("Select");
         orderSelector.addActionListener(new ActionListener(){
        
-           //@Override
            public void actionPerformed(ActionEvent e) {
                chooser.showDialog(pane, "Select as Order File");
 
-               
                int retVal = chooser.showOpenDialog(pane);
                 if (retVal == JFileChooser.APPROVE_OPTION) {
                     File selectedfile = chooser.getSelectedFile();
@@ -90,6 +110,9 @@ public class Interface {
 
        });
 
+       //specified parameter input for Local Beam Search and Random-Restart Hill-Climbing
+       //JTextField nrInput = new JTextField(20);
+      // nrInput.setEditable(true);
 
        //create Layout and Elements
         pane.setLayout(new FlowLayout());
@@ -97,24 +120,23 @@ public class Interface {
         //fügt Labels (Schilder) und buttons hinzu
         pane.add(new JLabel("Warehouse Information:", JLabel.CENTER));
         pane.add(warehouseSelector); //Button
-        
         pane.add(new JLabel("Order:", JLabel.CENTER));
-        pane.add(orderSelector);
+        pane.add(orderSelector); //Button
         pane.add(new JLabel("Select Algorithm", JLabel.CENTER));
         pane.add(cBox);
         pane.add(runButton);
-        
-       pane.add(new JLabel("Solution: "));
-       //muss noch angepasst werden
-       JLabel ausgabe = new JLabel("...");
-       ausgabe.setPreferredSize(new Dimension(600, 600));
-       ausgabe.setBorder(new LineBorder(Color.BLACK));
-       ausgabe.setBackground(Color.WHITE);
-       ausgabe.setOpaque(true);
-       pane.add(ausgabe);
+        pane.add(new JLabel("Solution: "));
 
-       pane.add(warehouseLabel);
-       pane.add(orderLabel);
+        //print out solution: muss noch angepasst werden
+        JLabel ausgabe = new JLabel("...");
+        ausgabe.setPreferredSize(new Dimension(900, 600)); //width and height
+        ausgabe.setBorder(new LineBorder(Color.BLACK));
+        ausgabe.setBackground(Color.WHITE);
+        ausgabe.setOpaque(true);
+        pane.add(ausgabe);
+
+        pane.add(warehouseLabel);
+        pane.add(orderLabel);
     }
 
     /**
@@ -130,8 +152,7 @@ public class Interface {
         JFrame frame = new JFrame("Warehouse Planning");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
-        
-    
+            
         //Set up the content pane.
         addComponentsToPane(frame.getContentPane());
 
