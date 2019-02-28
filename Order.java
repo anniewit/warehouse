2b59ca9b824ff0324f7da401c63f7d3a3e8e9cdd
anniewit <annemarie.witschas@gmail.com>
2019-02-28 17:23:01 +0100

    noch nicht fertig

--- Order.java
+++ Order.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.util.Scanner;
 
 /**
- * Object that stores input information from user and further calculations and gives access from several files
+ * Object that stores input information from user and Zwischenschritte and gives access from several files
  */
 public class Order {
     
@@ -39,128 +39,5 @@ public class Order {
     }
     
 
-
-/*
- 
-    public BufferedReader read(File file) throws FileNotFoundException {
-        //BufferedReader ließt Warenhaus ein:
-        //File file = new File("C:\\Users\\Julia Stocker\\Javalernen\\AI Programming Project\\problem1.txt");
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        return br;
-    }
-
-    private HashMap<Integer,String> warehouseReader()throws Exception, FileNotFoundException{
-            //BufferedReader ließt Warenhaus ein:
-            //warehousePath = ".\\problem1.txt";
-            File whFile = new File(warehousePath);
-            BufferedReader br = this.read(whFile);
-            //BufferedReader br = new BufferedReader(new FileReader(whFile));
-
-            //nur Items werden eingelesen
-            String[] items = br.readLine().split(" ");
-            //Items werden zu Zahlen gemappt
-            HashMap<Integer,String> itemsInMap = new HashMap<Integer,String>();
-            //Items bekommen einen Identifier:
-            //i = itemPostition
-            for(int i = 0; i <= items.length - 1; i++){
-                itemsInMap.put(i, items[i]);
-            }
-        return(itemsInMap);
-
-    }
-
-    private HashSet<String> orderReader() throws Exception, FileNotFoundException{
-            //Order wird durch Scanner eingelesen:
-            File ordFile =  new File(orderPath);
-            Scanner sc = new Scanner(ordFile);
-
-            //HashSet für eingelesene order
-            HashSet<String> order = new HashSet<>();
-
-            //Eingelesene items werden dem HashSet zugefügt
-            while(sc.hasNext()){
-                order.add(sc.next().trim());
-            }
-
-            sc.close();
-
-        return order;
-    }
-
-
-    private ArrayList<Integer> gettingTheOrder() throws Exception{
-        HashMap<Integer,String> itemsInMap = warehouseReader();
-        HashSet order = orderReader();
-
-        //ArrayList für Keys of items
-        ArrayList<Integer> orderlist = new ArrayList<Integer>();
-
-
-        //Items in HashSet werden mit items im Warenhaus verglichen
-        //Keys der Items in dem Order werden in ArrayList gespeichert
-        for(Map.Entry<Integer,String> entry : itemsInMap.entrySet()){
-            if(order.contains(entry.getValue()))
-                orderlist.add(entry.getKey());
-        }
-        return orderlist;
-    }
-
-
-    public HashMap<Integer,ArrayList<Integer>> gettingPSUs() throws Exception, FileNotFoundException{
-        String line; //Eine Zeile in dem Warenhaus
-        int id = 1000;//Einen identifier für die PSUs
-        File whFile = new File(warehousePath);
-        BufferedReader br = read(whFile);
-        HashMap<Integer, String> itemsInMap = warehouseReader();
-        //Initalisierung der ArrayList<Integer>, darin werden die items gespeichert
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        //Initialisierung einer neuen HashMap,die die identifieres als keys und die items in listen als values hat.
-        HashMap<Integer,ArrayList<Integer>> psus = new HashMap<>();
-
-        //Diese Schleife durchläuft das Warenhaus Zeile für Zeile
-        while((line = br.readLine()) != null){
-        //Dann wird auf die items in der HashMap durch Map.Entry zugegriffen
-            for(Map.Entry<Integer,String> entry : itemsInMap.entrySet()){
-                //Falls item der Zeile in der HashMap ist wird dieses in ArrayList gespeichert
-                if(line.contains(entry.getValue())){
-                    list.add(entry.getKey());
-                }
-            //eine neue Liste wird angefertigt und in der HashMap gespeichert
-            psus.put(id, new ArrayList<Integer>(list));
-            }
-        //die identifieres werden hochgezählt
-        id++;
-        //die liste wird gelöscht um neue Elemente zu speichern
-        list.clear();
-        }
-
-        return psus;
-    }
-
-    public HashMap reducePsus() throws Exception{
-
-   //     Preprocessing access = new Preprocessing();
-
-        HashMap<Integer,String> itemsInMap = this.warehouseReader();
-        ArrayList<Integer> orderlist = this.gettingTheOrder();
-        HashMap<Integer,ArrayList<Integer>> psus = this.gettingPSUs();
-
-        //Neue HashMap für relevante PSUs erstellen:
-        HashMap<Integer,ArrayList<Integer>> relevantPSUs = new HashMap<>();
-
-        //Order wird mit den PSU-items verglichen und in Hashmap gespeichert:
-        for(Map.Entry<Integer,ArrayList<Integer>> entry: psus.entrySet()) {
-            ArrayList<Integer> items = entry.getValue();
-            items.retainAll(orderlist);
-            //PSUs werden gelöscht, die keine items haben.
-            if(!items.isEmpty()){
-                relevantPSUs.put(entry.getKey(), items);
-            }
-        }
-        return relevantPSUs;
-        //System.out.println(relevantPSUs);
-
-
-    } */
 
 }
