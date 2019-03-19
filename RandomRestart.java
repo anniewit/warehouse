2b7b281066093c9866790b3ff06339fd438339c3
juliaelina <46715822+juliaelina@users.noreply.github.com>
2019-03-19 13:37:32 +0100

    Add files via upload

--- RandomRestart.java
+++ RandomRestart.java
@@ -7,22 +7,33 @@ import java.nio.*;
 
 public class RandomRestart {
 
-    public static void main(String[] args){
-        Interface Interface = new Interface();
-        n = Interface.getInput();
-        int[] state_values = new [n];
+    public boolean [] randomRestart(Order o) throws Exception{
+        /**
+        * @ param   n               saves the number of HillClimbing runtimes
+        *           state_value     gets the value return of one HillClimbing runtime
+        *           state_values    saves all these values in an array
+        */
+        int n = Order.getRep();
+        int[] state_values = new int[n];
+
+        boolean[] currentState = HillClimbing.bestState(o, currentState);
+        int state_value = StateMethods.countPsus(currentState);
+        // at position zero state value of one hillClimbing search is saved.
+        state_values[0] = state_value;
 
         for(int i = 1; i <= n; i++){
-            HillClimbing hillClimbing = new HillClimbing();
-            boolean[] highestNeighbour = hillClimbing.bestState();
-            int state_value = hillClimbing.countPsus(highestNeighbour);
-            state_values[0] = state_value;
-            if(state_value[i] > state_values[0]){
-                state_values[0] = state_value[i];
+            boolean[] someNeighbour = HillClimbing.hillclimbing(o);
+            state_values[i] = StateMethods.countPsus(someNeighbour);
+            // check whether next hillClimbing value is better, if yes save at position 0
+            if(state_values[i] > state_values[0]){
+                state_values[0] = state_values[i];
+                System.arraycopy(someNeighbour,0,currentState,0,currentState.length);
             }
 
         }
+        return currentState;
         // highestValue = state_values[0];
+
 
 
 
